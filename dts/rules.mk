e30559fe5b62e8db40359561f51bc5335eb526c9
thinhx2 <nthinh1102@gmail.com>
2020-07-17 04:26:27 +0700

    msm8929: Y91: initial bringup to lk2nd

--- dts/rules.mk
+++ dts/rules.mk
@@ -21,5 +21,6 @@ DTBS += \
 	$(LOCAL_DIR)/msm8916-samsung-r08.dtb \
 	$(LOCAL_DIR)/msm8929-samsung-r00.dtb \
 	$(LOCAL_DIR)/msm8929-samsung-r04.dtb \
+	$(LOCAL_DIR)/msm8929-coolpad-Y91.dtb \
 	$(LOCAL_DIR)/msm8939-mtp.dtb
 endif
